3111542968e5a0c4cf5bd75f1010f800ddd672e7
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-04-29 20:41:24 -0300

    1 file modified, 2 added

---
+++ src/app/thaylla-lista/thaylla-lista.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thaylla-lista',
+  templateUrl: './thaylla-lista.component.html',
+  styleUrls: ['./thaylla-lista.component.css']
+})
+export class ThayllaListaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
