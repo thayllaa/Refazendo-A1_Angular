6f99201eb7f2df33956a4346e81131627ac6f5ea
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-04-29 20:46:48 -0300

    1 file modified

--- src/app/thaylla-lista/thaylla-lista.component.ts
+++ src/app/thaylla-lista/thaylla-lista.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThayllaDisciplinasService } from '../thaylla-disciplinas.service';
 
 @Component({
   selector: 'app-thaylla-lista',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThayllaListaComponent implements OnInit {
 
-  constructor() { }
+  constructor(public lista: ThayllaDisciplinasService) { }
 
   ngOnInit() {
   }
