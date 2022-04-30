b29aa17a0d788cd2f97d826ce5f5c68f0d9b670f
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-04-29 22:49:29 -0300

    2 files modified

--- src/app/thaylla-disciplinas.service.ts
+++ src/app/thaylla-disciplinas.service.ts
@@ -11,7 +11,57 @@ interface Disciplinas {
 @Injectable()
 export class ThayllaDisciplinasService {
 
-  lista: Array<Disciplinas> = [];
+  lista: Array<Disciplinas> = [
+    {
+      id: "IBD006",
+      name: "Banco de Dados",
+      day: "Quarta-feira",
+      hour: "9h30 - 13h",
+      ement: "",
+    },
+    {
+      id: "IDS001",
+      name: "Desenvolvimento para Servidores I",
+      day: "Segunda-feira",
+      hour: "9h30 - 13h",
+      ement: "",
+    },
+    {
+      id: "IDS002",
+      name: "Desenvolvimento para Servidores II",
+      day: "Quinta-feira",
+      hour: "19h - 22h30",
+      ement: "",
+    },
+    {
+      id: "ISW012",
+      name: "Desenvolvimento para Dispositivos Móveis I",
+      day: "Segunda-feira",
+      hour: "19h - 22h30",
+      ement: "",
+    },
+    {
+      id: "AGM004",
+      name: "Negócios, Marketing, Eletrônicos",
+      day: "Quarta-feira",
+      hour: "19h - 22h30",
+      ement: "",
+    },
+    {
+      id: "IWS011",
+      name: "Projeto de Prototipagem e Testes de Usabilidade",
+      day: "Sabado",
+      hour: "8h - 11h30",
+      ement: "",
+    },
+    {
+      id: "ITE004",
+      name: "Tópicos Especiais II",
+      day: "Sexta-feira",
+      hour: "19h - 22h30",
+      ement: "",
+    },
+  ];
  
   constructor() { }
 
